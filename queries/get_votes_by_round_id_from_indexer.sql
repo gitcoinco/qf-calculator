c8ed83b63d731e42b250586ece56c7f6546a380e
Umar Khan <ufkhan97@gmail.com>
2024-05-21 12:55:48 -0600

    update indexer db

--- queries/get_votes_by_round_id_from_indexer.sql
+++ queries/get_votes_by_round_id_from_indexer.sql
@@ -14,8 +14,8 @@ SELECT
     d.amount_in_usd as "amountUSD",
     d.amount_in_round_match_token
 FROM
-    "chain_data_63".donations d
-LEFT JOIN "chain_data_63".applications a 
+    "chain_data_66".donations d
+LEFT JOIN "chain_data_66".applications a 
   ON 
     a.round_id = d.round_id AND 
     a.chain_id = d.chain_id AND
