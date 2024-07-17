c4668ad5fedcb61824849bcac6e59f47b6ad7aae
Umar Khan <ufkhan97@gmail.com>
2024-07-17 17:17:07 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -548,3 +548,187 @@ fig.update_layout(
 
 # Display the bar graph
 st.plotly_chart(fig)
+
+### TEMPORARY CODE FOR TESTING
+def get_exclusive_voters(df):
+    # Calculate the total voters and exclusive voters for each title
+    total_voters = {}
+    exclusive_voters = {}
+    # Group the dataframe by voter to get the titles each voter voted for
+    voter_groups = df.groupby('voter')['project_name'].apply(set)
+    project_names = df['project_name'].unique().tolist()
+    for i, project in enumerate(project_names):
+        voters = set(df[df['project_name'] == project]['voter'])
+        total_voters[project_names[i]] = len(voters)
+        # Voters who voted for this title only
+        exclusive_voters_count = 0
+        for voter in voters:
+            if voter_groups[voter] == {project}:
+                exclusive_voters_count += 1        
+        exclusive_voters[project_names[i]] = exclusive_voters_count
+
+    # Calculate the percentage of exclusive voters
+    exclusive_voters_percentage = {project: exclusive_voters[project] / total_voters[project] * 100 for project in project_names}
+
+    # Create DataFrame for plotting
+    plot_df = pd.DataFrame({
+        'Project': list(exclusive_voters_percentage.keys()),
+        'Exclusive Voters (%)': list(exclusive_voters_percentage.values()),
+        'Total Voters': list(total_voters.values())
+    })
+
+    # Sort DataFrame by 'Exclusive Voters (%)' in descending order
+    plot_df = plot_df.sort_values('Exclusive Voters (%)', ascending=False)
+
+    # create a truncated project_names column for display in the graph
+    plot_df['Project_Short'] = plot_df['Project'].apply(lambda x: x[:20] + '...' if len(x) > 20 else x)
+    # Create a bar plot using plotly
+    fig = px.bar(plot_df.head(20), x='Project_Short', y='Exclusive Voters (%)', color='Exclusive Voters (%)', 
+                title='Projects with the Most Exclusive Voters',
+                color_continuous_scale='reds',
+                hover_data={'Total Voters': True, 'Exclusive Voters (%)': ':.2f'})
+    
+    return fig, plot_df
+
+st.write(df.head())
+fig, plot_df = get_exclusive_voters(df)
+
+st.plotly_chart(fig, use_container_width=True)
+
+
+
+### 
+st.header('Network Graph of Voters and Grants')
+import networkx as nx
+import time
+
+color_toggle = st.checkbox('Toggle colors', value=True)
+
+dfv = df.groupby(['voter', 'project_name'], as_index=False)['amountUSD'].sum()
+dfv = dfv.rename(columns={'voter': 'voter_id', 'project_name': 'title', 'amountUSD': 'amountUSD'})
+
+min_donation = st.slider('Minimum Donation Amount (USD)', min_value=0, max_value=10, value=0)
+
+# Filter the dataframe based on the minimum donation amount
+dfv_filtered = dfv[dfv['amountUSD'] >= min_donation]
+
+
+if color_toggle:
+    grants_color = '#00433B'
+    grantee_color_string = 'moss'
+    voters_color = '#C4F092'
+    voter_color_string = 'lightgreen'
+    line_color = '#6E9A82'
+else:
+    grants_color = '#FF7043'
+    grantee_color_string = 'orange'
+    voters_color = '#B3DE9F'
+    voter_color_string = 'green'
+    line_color = '#6E9A82'
+
+
+st.markdown('**- Tip: Go fullscreen with the arrows in the top-right for a better view.**')
+# Initialize a new Graph
+B = nx.Graph()
+
+# Create nodes with the bipartite attribute
+B.add_nodes_from(dfv['voter_id'].unique(), bipartite=0, color=voters_color) 
+B.add_nodes_from(dfv['title'].unique(), bipartite=1, color=grants_color) 
+
+
+
+# Add edges with amountUSD as an attribute
+for _, row in dfv.iterrows():
+    B.add_edge(row['voter_id'], row['title'], amountUSD=row['amountUSD'])
+
+
+
+# Compute the layout
+current_time = time.time()
+pos = nx.spring_layout(B, dim=3, k = .09, iterations=50)
+new_time = time.time()
+
+
+    
+# Extract node information
+node_x = [coord[0] for coord in pos.values()]
+node_y = [coord[1] for coord in pos.values()]
+node_z = [coord[2] for coord in pos.values()] # added z-coordinates for 3D
+node_names = list(pos.keys())
+# Compute the degrees of the nodes 
+degrees = np.array([B.degree(node_name) for node_name in node_names])
+# Apply the natural logarithm to the degrees 
+log_degrees = np.log(degrees + 1)
+# Min-Max scaling manually
+#min_size = 10  # minimum size
+#max_size = 50  # maximum size
+#node_sizes = ((log_degrees - np.min(log_degrees)) / (np.max(log_degrees) - np.min(log_degrees))) * (max_size - min_size) + min_size
+node_sizes = log_degrees * 10
+
+# Extract edge information
+edge_x = []
+edge_y = []
+edge_z = []  
+edge_weights = []
+
+for edge in B.edges(data=True):
+    x0, y0, z0 = pos[edge[0]]
+    x1, y1, z1 = pos[edge[1]]
+    edge_x.extend([x0, x1, None])
+    edge_y.extend([y0, y1, None])
+    edge_z.extend([z0, z1, None])  
+    edge_weights.append(edge[2]['amountUSD'])
+
+# Create the edge traces
+edge_trace = go.Scatter3d(
+    x=edge_x, y=edge_y, z=edge_z, 
+    line=dict(width=1, color=line_color),
+    hoverinfo='none',
+    mode='lines',
+    marker=dict(opacity=0.5))
+
+
+# Create the node traces
+node_trace = go.Scatter3d(
+    x=node_x, y=node_y, z=node_z,
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        color=[data['color'] for _, data in B.nodes(data=True)],  # color is now assigned based on node data
+        size=node_sizes,
+        opacity=1,
+        sizemode='diameter'
+    ))
+
+
+node_adjacencies = []
+for node, adjacencies in enumerate(B.adjacency()):
+    node_adjacencies.append(len(adjacencies[1]))
+node_trace.marker.color = [data[1]['color'] for data in B.nodes(data=True)]
+
+
+# Prepare text information for hovering
+node_trace.text = [f'{name}: {adj} connections' for name, adj in zip(node_names, node_adjacencies)]
+
+# Create the figure
+fig = go.Figure(data=[edge_trace, node_trace],
+                layout=go.Layout(
+                    title='3D Network graph of voters and grants',
+                    titlefont=dict(size=20),
+                    showlegend=False,
+                    hovermode='closest',
+                    margin=dict(b=20,l=5,r=5,t=40),
+                    annotations=[ dict(
+                        showarrow=False,
+                        text="This graph shows the connections between voters and grants based on donation data.",
+                        xref="paper",
+                        yref="paper",
+                        x=0.005,
+                        y=-0.002 )],
+                    scene = dict(
+                        xaxis_title='X Axis',
+                        yaxis_title='Y Axis',
+                        zaxis_title='Z Axis')))
+                        
+st.plotly_chart(fig, use_container_width=True)
+st.caption('Time to compute layout: ' + str(round(new_time - current_time, 2)) + ' seconds')
