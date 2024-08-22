fd73aea261da540c1218fce07d5553f576fba68a
Umar Khan <ufkhan97@gmail.com>
2024-08-22 02:49:17 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -413,7 +413,7 @@ def adjust_matching_overflow(output_df, matching_funds_available, matching_token
     full_matching_funds_available = int(int(matching_funds_available) * 10**(int(matching_token_decimals)))
     matching_overflow = sum(int(x) for x in output_df['matched']) - full_matching_funds_available
     while matching_overflow >= 0:
-        st.warning('Potential Matching Overflow Detected. Adjusting Matching Funds')
+        st.warning(f'Potential Matching Overflow of {matching_overflow} Detected. Adjusting Matching Funds')
         output_df['matched_pct'] = output_df['matched'] / output_df['matched'].sum()
         output_df['matched'] = (output_df['matched'] - (output_df['matched_pct'] * matching_overflow).apply(lambda x: int(x))).clip(lower=1)
         matching_overflow = sum(int(x) for x in output_df['matched']) - full_matching_funds_available
